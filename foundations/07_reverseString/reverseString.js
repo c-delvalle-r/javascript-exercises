d09a3477549ca301968791a8cda25b8dd96d2975
Carlos Dv <delvallereyes.carlos@gmail.com>
2026-08-13 14:19:22 -0600

    Complete exercise 7 of js foundations

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    characters = string.split('');
+    charactersReversed = [];
+    for (let i = 1 ; i<=characters.length; i++) {
+        charactersReversed.push(characters.at(-i));
+    }
+    return charactersReversed.join('');
 };
 
 // Do not edit below this line
