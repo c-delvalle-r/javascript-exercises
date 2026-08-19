0ae2a35694ce4f5171b74def88ecebfdbe625dbb
Carlos Dv <delvallereyes.carlos@gmail.com>
2026-08-18 20:40:03 -0600

    Complete exercise 8 of js foundations

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+// Usar splice
+    let itemsToDelete = Array.from(arguments);
+    itemsToDelete.shift();
+
+    for (item of itemsToDelete) {
+        let index = arr.indexOf(item);
+
+        // if (index === -1) {
+        //     continue;
+        // } else {
+        //     arr.splice(index, 1)
+        // }
+
+        while (index !== -1) {
+            // console.log(index);
+            arr.splice(index, 1);
+            index = arr.indexOf(item);
+        }
+    }
+
+    return arr;
+    // Iterar sobre cada argumento excepto arr)
+    // Encontrar el indice donde haya un match
+    // Si el resultado es undefined, no hay match
+    // Usar splice index, 1 para eliminar
 };
+
+console.log(removeFromArray([1,2,2,3], 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
