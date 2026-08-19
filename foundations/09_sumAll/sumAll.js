979ed39e06b2eac60cefed5e78a7c2d851f721cf
Carlos Dv <delvallereyes.carlos@gmail.com>
2026-08-18 21:00:05 -0600

    Complete exercise 9 of js foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // Validate arguments, otherwise return error
+    function validInput(input) {
+        return Number.isInteger(input) & input >= 0
+    }
 
+    if (!validInput(num1) || !validInput(num2)) {
+        return 'ERROR';
+    }
+
+    // Determine smaller number and bigger number
+    let smallerNumber = Math.min(num1, num2);
+    let biggerNumber = Math.max(num1, num2);
+
+    // Sum all number in between
+    let sum = 0;
+    for (let i = smallerNumber; i <= biggerNumber ; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+console.log(sumAll(2,4));
 
 // Do not edit below this line
 module.exports = sumAll;
