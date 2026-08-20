a3a8f465b98ad552b0b85315ed304492629430bb
Carlos Dv <delvallereyes.carlos@gmail.com>
2026-08-19 18:26:35 -0600

    Complete exercise 11 of js foundations

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFarenheit) {
+  return Math.round((degreesFarenheit - 32) / 1.8 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  return Math.round(((degreesCelsius * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
