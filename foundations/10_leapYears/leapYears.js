7ef8ecaffbc8d896be21aefd6b2ab0bf8ed3bc07
Carlos Dv <delvallereyes.carlos@gmail.com>
2026-08-19 18:09:53 -0600

    Complete exercise 10 of js foundations

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(yearNumber) {
+    if (yearNumber % 100 === 0 && yearNumber % 400 === 0) {
+        return true;
+    } else if (yearNumber % 100 !== 0 && yearNumber % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
